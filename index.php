06805ddcf93d68b94a891d77cc3a0107b8b88762
BrandonsCode <brandonscpucode@gmail.com>
2022-05-26 00:53:54 -0400

    The Finish Code

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<html>
+<head><title></title></head>
 <body>
     <?php
 
     $DBHOST = "localhost";
     $DBUSER = "root";
     $DBPWD = "";
-    $DBNAME = "test_db"
+    $DBNAME = "test_db";
 
-    $conn = new mysqli($DBHOST, $DBUSER ,$DBPWD, $DBNAME);
+    $conn = new mysqli($DBHOST,  $DBUSER,  $DBPWD,  $DBNAME);
 
+    
+    if($conn->connect_error)
+    {
+        die("Connection failed:" .$conn->connect_error);
+    }
 
-    echo "This is our page!";
+    echo "Connection successfully";
 
     
     ?>
---
+++ insert.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+$DBHOST = "localhost";
+$DBUSER = "root";
+$DBPWD = "";
+$DBNAME = "test_db";
+
+$conn = new mysqli($DBHOST, $DBUSER, $DBPWD, $DBNAME);
+
+if($conn->connect_error)
+{
+
+die("Connection failed":. $conn->connect_error);
+
+}
+
+$statement = "INSERT INTO name VALUES('$_POST[first_name]','$_POST[last_name]')";
+
+$conn->query($statement);
+
+$conn->close();
+
+?>
