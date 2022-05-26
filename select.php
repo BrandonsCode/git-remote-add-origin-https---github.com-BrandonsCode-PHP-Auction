b690d222dd75573f495119bc6512de0b7e5ba4d0
BrandonsCode <brandonscpucode@gmail.com>
2022-05-26 00:53:30 -0400

    finish code

---
+++ select.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+$DBHOST = "localhost";
+$DBUSER = "root";
+$DBPWD = "";
+$DBNAME = "test_db";
+
+$conn = new mysqli($DBHOST, $DBUSER, $DBPWD, $DBNAME);
+
+if($conn->connect_error)
+{
+
+    die("Connection failed":. $conn->connect_error);
+
+}
+
+$statement = "SELECT * FROM name";
+$conn->query($statement);
+while($row = $sql->fetch_assoc())
+{
+
+}
+
+?>
